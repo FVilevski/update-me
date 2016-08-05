0ca4e801550488fc0c157a4b6d0c91f58531254c
Jovan Panev <dodo_panev@yahoo.com>
2016-08-05 14:31:22 +0200

    Updater added

---
+++ UpdateMe.Updater/AppUpdater.cs
@@ -0,0 +1,67 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateMe.Updater
+{
+    public class AppUpdater : IAppUpdater
+    {
+        NuGet.SemanticVersion _currentVersion;
+        public NuGet.SemanticVersion CurrentVersion
+        {
+            get
+            {
+                if (_currentVersion == null)
+                {
+                    _currentVersion = _updateManager.CurrentlyInstalledVersion();
+                }
+                return _currentVersion;
+            }
+        }
+
+        private readonly IUpdateManager _updateManager;
+
+        public AppUpdater(IUpdateManager updateManager)
+        {
+            _updateManager = updateManager;
+        }
+
+        public async Task<bool> HasNewUpdate()
+        {
+            bool hasUpdate = false;
+            try
+            {
+                UpdateInfo info = await _updateManager.CheckForUpdate();
+
+                hasUpdate = !this.CurrentVersion.Equals(info.FutureReleaseEntry.Version);
+
+            }
+            catch (System.Net.WebException ex)
+            {
+                System.Diagnostics.Trace.WriteLine($"HasNewUpdate failed with web expetion {ex.Message}");
+                hasUpdate = false;
+            }
+            return hasUpdate;
+        }
+
+        public async Task UpdateApp(Action<int> progress = null)
+        {
+            try
+            {
+                if (await this.HasNewUpdate())
+                {
+                    ReleaseEntry info = await _updateManager.UpdateApp(progress);
+                    _updateManager.Dispose();
+                    UpdateManager.RestartApp();
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                System.Diagnostics.Trace.WriteLine($"UpdateApp failed with web expetion {ex.Message}");
+            }
+        }
+    }
+}
---
+++ UpdateMe.Updater/IAppUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateMe.Updater
+{
+    public interface IAppUpdater
+    {
+        NuGet.SemanticVersion CurrentVersion
+        {
+            get;
+        }
+
+        Task<bool> HasNewUpdate();
+
+        Task UpdateApp(Action<int> progress = null);
+    }
+}
